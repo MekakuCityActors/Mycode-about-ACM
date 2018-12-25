ef58593aa8f00040e27116388d83cebf7eaf7589
MekakuCityActors <33919397+MekakuCityActors@users.noreply.github.com>
2018-12-25 20:14:12 +0800

    Update 1010.cpp
    斜率优化dp
    问题如果具有决策单调性时考虑斜率优化dp和单调栈优化dp，再根据解最优时的关系(即最优决策k与普通决策j存在的关系)考虑使用哪种方式进行优化。

--- 1010.cpp
+++ 1010.cpp
@@ -3,8 +3,8 @@
     User: MekakuCityActors
     Language: C++
     Result: Accepted
-    Time:180 ms
-    Memory:2852 kb
+    Time:164 ms
+    Memory:2856 kb
 ****************************************************************/
 #include<iostream>
 #include<cstring>
@@ -20,8 +20,8 @@ typedef long long ll;
 ll dp[50005],qwq[50005];
 ll sum[50005],s[50005];
 ll n,L;
-ll slope(int x,int y){
-    return (dp[y]-dp[x]+sum[y]*sum[y]-sum[x]*sum[x]+2*(L+1)*(sum[y]-sum[x]))/(2.0*(sum[y]-sum[x]));
+ll slope(int x,int y){//注意要把斜率写成这个形式（把分母写在函数外面与式子右端的式子相乘）来防止分母为0
+    return (dp[y]-dp[x]+sum[y]*sum[y]-sum[x]*sum[x]+2*(L+1)*(sum[y]-sum[x]));
 }
 int main(){
   //freopen("in.txt","r",stdin);
@@ -37,11 +37,11 @@ int main(){
     int r=1;
     s[1]=0;
     for(int i=1;i<=n;i++){
-        while(l<r&&slope(s[l],s[l+1])<=sum[i]){
+        while(l<r&&slope(s[l],s[l+1])<=sum[i]*(2.0*(sum[s[l+1]]-sum[s[l]]))){//把分母写在右端相乘的形式以防止分母为0
             l++;
         }
         dp[i]=dp[s[l]]+(sum[i]-sum[s[l]]-(L+1))*(sum[i]-sum[s[l]]-(L+1));
-        while(r>l&&slope(s[r],i)<slope(s[r-1],s[r])){
+        while(r>l&&slope(s[r],i)*(2.0*(sum[s[r]]-sum[s[r-1]]))<=slope(s[r-1],s[r])*(2.0*(sum[i]-sum[s[r]]))){//注意弹出的时候要使用<=而不能使用<
             r--;
         }
         s[++r]=i;
@@ -49,4 +49,3 @@ int main(){
     cout << dp[n] << endl;
     return 0;
 }
-﻿
